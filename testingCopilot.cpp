767b255d8c06897cd1cc4df632c4f6dc363c1029
juuubsbs <julianasimon@edu.univali.br>
2024-06-01 17:30:38 -0300

    array?

--- testingCopilot.cpp
+++ testingCopilot.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main() {
-    string name;
-    cout << "Enter your name: ";
-    cin >> name;
-    cout << "Hello, " << name << "!" << endl;
+    
+    int letters, counter;
+    
+    std:: cout << "Enter the number of letters on your name: ";
+    std:: cin >> letters;
+    char name[letters];
+
+    for(counter = 0; counter < letters; counter++) {
+        std:: cout << "Enter the letter " << counter + 1 << " of your name: " ;
+        std:: cin >> name[counter];
+    }
+
+    std:: cout << "Your name is: ";
+    counter = 0;
+
+    for (counter = 0; counter < letters; counter++){
+        std::cout << name[counter];
+
+    }
+    std:: cout << std::endl;
+
+
+    system ("pause");
     return 0;
 }
